16981df81ef2ec5002dc0cc290b39783ae72e6a3
Bas van Dijk <bas@usto.nl>
2017-07-20 17:53:27 +0200

    Added deviceId switching

--- src/switch/switchController.js
+++ src/switch/switchController.js
@@ -3,12 +3,12 @@
 let domusto = require('../domusto.js');
 
 exports.on = function (req, res) {
-    res.json('on');
-    domusto.switchOn();
+    res.json('on');   
+    domusto.switchOn(req.params.deviceId);
 };
 
 exports.off = function (req, res) {
     // console.log(req);
     res.json('off');
-    domusto.switchOff();
+    domusto.switchOff(req.params.deviceId);
 };
