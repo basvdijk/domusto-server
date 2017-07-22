31650839a1014c4a7ed3b8aa3abe7e0a8d58b55c
Bas van Dijk <bas@usto.nl>
2017-07-22 11:59:02 +0200

    Added CORS headers

--- src/server.js
+++ src/server.js
@@ -4,10 +4,34 @@ let io = require('socket.io')(server);
 
 let domusto = require('./domusto.js');
 let util = require('./util.js');
+let core = require('./core.js');
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 
 // Routes
 let switchRoutes = require('./switch/switchRoutes');
 let inputRoutes = require('./input/inputRoutes');
+let outputRoutes = require('./output/outputRoutes');
+let coreRoutes = require('./core/coreRoutes');
 
 domusto.init(io);
 
@@ -15,10 +39,14 @@ let port = process.env.PORT || 3000;
 
 switchRoutes(app);
 inputRoutes(app);
+outputRoutes(app);
+coreRoutes(app);
 
 server.listen(port, function () {
     util.log('Domusto REST api server started on: ' + port);
 });
+
+core.data.port = port;
 
 // io.on('connection', function (socket) {
 //     socket.emit('stream', { hello: 'world' });
