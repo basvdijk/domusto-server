fb54121e2997241f519110a019601907c20f0758
Bas van Dijk <bas@usto.nl>
2017-08-06 21:33:40 +0200

    Started with DOMUSTO shell plugin

---
+++ src/plugins/domusto-shell/index.js
@@ -0,0 +1,51 @@
+let exec = require('child_process').exec;
+let util = require('../../util');
+let config = require('../../config');
+
+let DomustoPlugin = require('../../domusto/domusto-plugin');
+
+/**
+ * Shell plugin for DOMUSTO
+ * @author Bas van Dijk
+ * @version 0.0.1
+ * 
+ * @class DomustoShell
+ * @extends {DomustoPlugin}
+ */
+class DomustoShell extends DomustoPlugin {
+
+    /**
+     * Creates an instance of DomustoShell.
+     * @param {any} Plugin configuration as defined in the config.js file 
+     * @memberof DomustoShell
+     */
+    constructor(pluginConfiguration) {
+
+        super({
+            plugin: 'Shell executer',
+            author: 'Bas van Dijk',
+            category: 'system',
+            version: '0.0.1',
+            website: 'http://domusto.com'
+        });
+
+    }
+
+    outputCommand(device, command, onSucces) {
+
+        if (device.state === 'on') {
+            console.log(device.protocol.actions.off);
+        } else {
+            console.log(device.protocol.actions.on);
+        }
+
+        onSucces({ state: device.state === 'off' ? 'on' : 'off' });
+
+    }
+
+    toString() {
+        return super.toString();
+    }
+}
+
+module.exports = DomustoShell;
