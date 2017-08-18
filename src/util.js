29c53fc93f4c3ba856c6df26bd8a4c971f31db3f
Bas van Dijk <bas@usto.nl>
2017-08-18 13:40:00 +0200

    Fixed typo in day names

--- src/util.js
+++ src/util.js
@@ -32,16 +32,24 @@ Util.getLogDate = function () {
 
     let date = new Date();
 
-    let days = ['SUS', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    let days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
     let dayString = days[date.getDay()];
 
-    return '' + dayString + ' ' + date.getFullYear() + '-' + pad((date.getMonth() + 1)) + '-' + pad(date.getDate()) + ' ' + pad(date.getHours()) + ':' + pad(date.getMinutes()) + ':' + pad(date.getSeconds()) + '.' + date.getMilliseconds()
+    return '' + 
+    dayString + ' ' 
+    + date.getFullYear() + '-' 
+    + pad((date.getMonth() + 1)) + '-' 
+    + pad(date.getDate()) + ' ' 
+    + pad(date.getHours()) + ':' 
+    + pad(date.getMinutes()) + ':' 
+    + pad(date.getSeconds()) + '.' 
+    + ('00' + date.getMilliseconds()).slice(-3);
 }
 
 Util.logSwitchToFile = function (data) {
     var logStream = fs.createWriteStream('../logs/switches.log', { 'flags': 'a' });
     // use {'flags': 'a'} to append and {'flags': 'w'} to erase and write a new file
-    logStream.end(Util.getLogDate() + '   ' + data);
+    logStream.end(Util.getLogDate() + '   ' + data + '\n');
 }
 
 /**
@@ -50,29 +58,32 @@ Util.logSwitchToFile = function (data) {
  */
 Util.offsetDate = function (date, cronDateOffset) {
 
-    let cronDateSplitted = cronDateOffset.split(' ');
+    if (cronDateOffset) {
 
-    if (cronDateSplitted.length !== 6) {
-        console.error('invalid cron formatted date pattern');
+        let cronDateSplitted = cronDateOffset.split(' ');
+
+        if (cronDateSplitted.length !== 6) {
+            console.error('invalid cron formatted date pattern');
+        }
+
+        let secondsOffset = parseInt(cronDateSplitted[0]) || 0;
+        date.setSeconds(date.getSeconds() + secondsOffset);
+
+        let minuteOffset = parseInt(cronDateSplitted[1]) || 0;
+        date.setMinutes(date.getMinutes() + minuteOffset);
+
+        let hourOffset = parseInt(cronDateSplitted[2]) || 0;
+        date.setHours(date.getHours() + hourOffset);
+
+        let dayOffset = parseInt(cronDateSplitted[3]) || 0;
+        date.setDate(date.getDate() + dayOffset);
+
+        let monthOffset = parseInt(cronDateSplitted[4]) || 0;
+        date.setMonth(date.getMonth() + monthOffset);
+
+        let yearOffset = parseInt(cronDateSplitted[5]) || 0;
+        date.setFullYear(date.getFullYear() + yearOffset);
     }
-
-    let secondsOffset = parseInt(cronDateSplitted[0]) || 0;
-    date.setSeconds(date.getSeconds() + secondsOffset);
-
-    let minuteOffset = parseInt(cronDateSplitted[1]) || 0;
-    date.setMinutes(date.getMinutes() + minuteOffset);
-
-    let hourOffset = parseInt(cronDateSplitted[2]) || 0;
-    date.setHours(date.getHours() + hourOffset);
-
-    let dayOffset = parseInt(cronDateSplitted[3]) || 0;
-    date.setDate(date.getDate() + dayOffset);
-
-    let monthOffset = parseInt(cronDateSplitted[4]) || 0;
-    date.setMonth(date.getMonth() + monthOffset);
-
-    let yearOffset = parseInt(cronDateSplitted[5]) || 0;
-    date.setFullYear(date.getFullYear() + yearOffset);
 
     return date;
 
